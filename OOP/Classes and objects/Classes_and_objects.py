dfd99725d5153625a4a188d7dbd09e03facae197
Nikoloman <hectorvel52@gmail.com>
2022-06-25 14:50:06 -0500

    I modified the code, I put methods and attributes to the class Cookie and did a few demostrations of them

--- OOP/Classes and objects/Classes_and_objects.py
+++ OOP/Classes and objects/Classes_and_objects.py
@@ -1,8 +1,27 @@
 #the class is like a model to create objects
 class Cookie:
-    pass
+    chocolate = False
 
-lil_cookie = Cookie() #initialize an object with the Cookie class
-big_cookie = Cookie()
+    def __init__(self, flavor = None, shape = None):
+        self.flavor = flavor
+        self.shape = shape
 
-print(type(lil_cookie))
+        if flavor is not None and shape is not None:
+            print(f"A {flavor} cookie with the shape of a {shape} just baked!")
+        else:
+            print("Wtf bruh, there's no flavor and color")
+
+    def put_chocolate(self):
+        self.chocolate = True
+
+    def have_chocolate(self):
+        if self.chocolate == True:
+            print("I'm a cookie with chocolate :D")
+        else:
+            print("I'm a cookie with no chocolate D:")
+
+lil_cookie = Cookie("Vanilla", "Car") #initialize an object with the Cookie class
+
+print(lil_cookie.have_chocolate())
+lil_cookie.put_chocolate()
+print(lil_cookie.have_chocolate())
