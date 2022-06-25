ae0915e2381265385b3608786048d1afc3709a9a
Nikoloman <hectorvel52@gmail.com>
2022-06-25 15:52:42 -0500

    Just added a few comments to the code

--- OOP/Classes and objects/Classes_and_objects.py
+++ OOP/Classes and objects/Classes_and_objects.py
@@ -1,16 +1,19 @@
 #the class is like a model to create objects
 class Cookie:
-    chocolate = False
+    chocolate = False #Attribute of the class
 
+    #Constructor
     def __init__(self, flavor = None, shape = None):
+        #Body of the constructor
         self.flavor = flavor
         self.shape = shape
 
         if flavor is not None and shape is not None:
             print(f"A {flavor} cookie with the shape of a {shape} just baked!")
         else:
-            print("Wtf bruh, there's no flavor and color")
+            print("Wtf bruh, there's no flavor and shape")
 
+    #Methods
     def put_chocolate(self):
         self.chocolate = True
 
@@ -22,6 +25,7 @@ class Cookie:
 
 lil_cookie = Cookie("Vanilla", "Car") #initialize an object with the Cookie class
 
-print(lil_cookie.have_chocolate())
+#Calling methods of the Cookie class with "lil_cookie" as object
+print(lil_cookie.have_chocolate()) 
 lil_cookie.put_chocolate()
 print(lil_cookie.have_chocolate())
