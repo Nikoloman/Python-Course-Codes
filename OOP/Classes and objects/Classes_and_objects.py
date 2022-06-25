76128fb3484ccdc5e4c57afe5a8986ede314bd5b
Nikoloman <hectorvel52@gmail.com>
2022-06-25 13:39:59 -0500

    Creation of the class Cookie and 2 instances as example

---
+++ OOP/Classes and objects/Classes_and_objects.py
@@ -0,0 +1,8 @@
+#the class is like a model to create objects
+class Cookie:
+    pass
+
+lil_cookie = Cookie() #initialize an object with the Cookie class
+big_cookie = Cookie()
+
+print(type(lil_cookie))
