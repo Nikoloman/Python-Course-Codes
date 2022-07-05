e92c6899c78c153906ee7508bf765e71a13014b5
Nikoloman <hectorvel52@gmail.com>
2022-07-05 16:41:55 -0500

    Creating my own packages and installing them so I can access to them anywhere

---
+++ Modules/Packages/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="package",
+    version="0.1",
+    description="This is a package sample",
+    author="Nikolo",
+    author_email="hectorvel52@gmail.com",
+    scripts=[],
+    packages=["Package", "Package.Goodbye", "Package.Hello"]
+)
