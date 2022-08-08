9207b470ed77c44aaa24fef48cea638894f91cc8
Nikoloman <hectorvel52@gmail.com>
2022-08-08 16:26:47 -0500

    Just changing and trying some fonts and stuff

--- Modules/Standard libraries/Random.py
+++ Modules/Standard libraries/Random.py
@@ -8,11 +8,11 @@ print(random.randrange(10)) #Gives a random integer number between 0 and a given
 
 print(random.randrange(0, 101, 2)) #Gives a random int number between 0 and a given number and steps
 
-s = "Hello There! :D"
+s = "Hello There! :D" 
 print(random.choice(s)) #Gives a random character from a given string
 
 l = [1, 2, 3, 4, 5]
 print(random.choice(l)) #It also works with lists
-random.shuffle(l) #Shuffles a list
+random.shuffle(l) #Shuffles a list 
 print(l)
 print(random.sample(l, 2)) #Gives as much random numbers as wanted 
